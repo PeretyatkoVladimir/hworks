6a122e331e350850a94017d27931d264d056038a
PeretyatkoVladimir <voc@ukr.net>
2016-10-11 17:40:33 +0300

    Update Main.java

--- src/hw1/university/Main.java
+++ src/hw1/university/Main.java
@@ -1,5 +1,7 @@
 package hw1.university;
 
+import java.util.ArrayList;
+
 /**
  * Created by valdess on 09.10.16.
  */
@@ -12,17 +14,25 @@ public class Main {
         Student s3 = new BudgetStudent("Vova", 45, 3);
         Student s4 = new BudgetStudent("Alex", 45, 3);
 
-        Group g = new Group("ACO77", "Evgen");
-        g.addStudent(s1);
-        g.addStudent(s2);
-        g.addStudent(s3);
-        g.addStudent(s4);
+        Group group1 = new Group("ACO16", "Gorobec");
+        group1.addStudent(s1);
+        group1.addStudent(s2);
+        group1.addStudent(s3);
+        group1.addStudent(s4);
 
-        University u = new University("ArtCode");
-        u.addGroup(g);
+        Group group2 = new Group("ACO16", "Gorobec");
+        group2.addStudent(s1);
+        group2.addStudent(s2);
+        group2.addStudent(s3);
+        //group2.addStudent(s4);
+
+        System.out.println(group1.equals(group2));
+
+        University u = University.getInstance();
+        u.setName("ArtCode");
+        u.addGroup(group1);
 
         System.out.println(u);
-
 
     }
 
