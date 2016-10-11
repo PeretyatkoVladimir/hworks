6634c6d049a49f114431c8cf2c12ff03cf3d9472
PeretyatkoVladimir <voc@ukr.net>
2016-10-11 17:40:54 +0300

    Update Student.java

--- src/hw1/university/Student.java
+++ src/hw1/university/Student.java
@@ -15,19 +15,24 @@ public class Student {
 
     @Override
     public boolean equals(Object o) {
+
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
         Student student = (Student) o;
 
-        if (Double.compare(student.averageMark, averageMark) != 0) return false;
         return name.equals(student.name);
 
     }
 
-    public String getName() {
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 
+    public String getName() {
         return name;
+
     }
 
     @Override
