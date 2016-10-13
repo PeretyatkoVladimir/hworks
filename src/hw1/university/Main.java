92aa6753f079b7c820e4e49ffe9efd8a12f6fc34
PeretyatkoVladimir <voc@ukr.net>
2016-10-13 17:20:02 +0300

    Update Main.java

--- src/hw1/university/Main.java
+++ src/hw1/university/Main.java
@@ -9,10 +9,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student s1 = new ContractStudent("Petro", 45, 3);
-        Student s2 = new ContractStudent("Ivan", 45, 3);
-        Student s3 = new BudgetStudent("Vova", 45, 3);
-        Student s4 = new BudgetStudent("Alex", 45, 3);
+        Student s1 = new ContractStudent("Petro", 3.5, 3);
+        Student s2 = new ContractStudent("Ivan", 3.7, 3);
+        Student s3 = new BudgetStudent("Vova", 3.4, 3);
+        Student s4 = new BudgetStudent("Alex", 3.6, 3);
 
         Group group1 = new Group("ACO16", "Gorobec");
         group1.addStudent(s1);
@@ -23,7 +23,7 @@ public class Main {
         Group group2 = new Group("ACO16", "Gorobec");
         group2.addStudent(s1);
         group2.addStudent(s2);
-        group2.addStudent(s3);
+        //group2.addStudent(s3);
         //group2.addStudent(s4);
 
         System.out.println(group1.equals(group2));
@@ -31,6 +31,13 @@ public class Main {
         University u = University.getInstance();
         u.setName("ArtCode");
         u.addGroup(group1);
+
+        System.out.println(group1);
+        group1.sortByStudent();
+        System.out.println(group1);
+
+        group1.sortByAverageMark();
+        System.out.println(group1);
 
         System.out.println(u);
 
