273b09fed3b02083c63a8d06387535577b730504
valdess <voc@ukr.net>
2016-10-28 20:40:58 +0300

    test commit

--- src/main/java/hw2/library/exeptions/ReaderAlreadyExistException.java
+++ src/main/java/hw2/library/exeptions/ReaderAlreadyExistException.java
@@ -4,7 +4,7 @@ package hw2.library.exeptions;
  * Created by vperetyatko on 27.10.2016.
  */
 public class ReaderAlreadyExistException extends Exception {
-    public ReaderAlreadyExistException(String message) {
-        super(message);
+    public ReaderAlreadyExistException() {
+
     }
 }
